f6cfe935e74473ecd354a6c9166ba72af0d8fc43
wangxiaochuan <wangxiaochuan@corp.netease.com>
2018-08-28 15:17:07 +0800

    add upload report function

--- src/views/quality-report/index.vue
+++ src/views/quality-report/index.vue
@@ -1,31 +1,45 @@
 <template>
-  <el-table
-    :data="items"
-    border
-    stripe>
-    <el-table-column prop="id" label="ID" width="100"/>
-    <el-table-column prop="createTime" label="创建日期"/>
-    <el-table-column prop="reportDate" label="报告日期"/>
-    <el-table-column prop="reportDate" label="查看报告"/>
-    <el-table-column label="图片">
-      <template slot-scope="scope">
-        <img :src="scope.row.url" >
-      </template>
-    </el-table-column>
-    <el-table-column label="操作">
-      <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleView(scope, $index, scope.row)">查看</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div class="main">
+    <el-row/>
+    <el-row>
+      <ReportForm/>
+    </el-row>
+    <el-row class="table">
+      <el-table
+        :data="items"
+        border
+        stripe>
+        <el-table-column prop="id" label="ID" width="100"/>
+        <el-table-column prop="createTime" label="创建日期"/>
+        <el-table-column prop="reportDate" label="报告日期"/>
+        <el-table-column prop="reportDate" label="查看报告"/>
+        <el-table-column label="图片">
+          <template slot-scope="scope">
+            <img :src="scope.row.url">
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleView(scope, $index, scope.row)">查看
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-row>
+  </div>
 </template>
 
 <script>
 import { getAllQualityReport } from '@/api/qualityReport'
+import ReportForm from './ReportForm'
+
 export default {
   name: 'QualityReport',
+  components: {
+    ReportForm
+  },
   data() {
     return {
       items: []
@@ -33,7 +47,9 @@ export default {
   },
   created() {
     console.log('quality report created')
-    getAllQualityReport().then(response => { this.items = response.data.items })
+    getAllQualityReport().then(response => {
+      this.items = response.data.items
+    })
     console.log(this.items)
   },
   methods: {
@@ -45,5 +61,7 @@ export default {
 </script>
 
 <style scoped>
-
+  .main {
+    padding: 20px
+  }
 </style>
